c04e94d97782144525f4f78459eae606c5932d57
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-14 02:34:35 +0300

    Created atesting view

---
+++ ratings/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+
+def hello(request):
+   current_user = request.user
+   return render(request,'home.html', {"current_user": current_user})
