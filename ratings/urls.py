b0b3ad42c95bf392f530000877c4a50bd9bb5888
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-15 08:59:56 +0300

    Added url for posting a project

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.conf import settings
 from django.urls import path
 from . import views
@@ -5,7 +6,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    path('', views.hello, name = 'hello' )
+    path('', views.home, name = 'home' ),
+    path('profile/<int:id>', views.profile, name = 'profile'),
+    path('update/profile', views.update_profile, name = 'updateProfile'),
+    path('post/project', views.post_project, name = 'postProject')
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
