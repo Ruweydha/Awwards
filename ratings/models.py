65c8ab134ca0673eafff4816e61de42b9606c7c6
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-15 08:59:11 +0300

    Created Projects model and ratings model

--- ratings/models.py
+++ ratings/models.py
@@ -1,10 +1,11 @@
 from distutils.command.upload import upload
+from time import timezone
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE )
@@ -26,4 +27,12 @@ class Project(models.Model):
     description = models.CharField(max_length=500)
     image = models.ImageField(upload_to = 'projectImages/')
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    
+    project_url = models.URLField()
+    date_posted = models.DateField(auto_now_add=True)
+
+class Ratings(models.Model):
+    design = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    usability = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    content = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+        
+
