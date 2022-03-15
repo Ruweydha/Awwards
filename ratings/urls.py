6d08fa90c73eef5c78c7a6025f8e8283f410c475
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-16 00:25:22 +0300

    Url for adding a rating

--- ratings/urls.py
+++ ratings/urls.py
@@ -4,12 +4,17 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 
+import ratings
+
 
 urlpatterns=[
     path('', views.home, name = 'home' ),
     path('profile/<int:id>', views.profile, name = 'profile'),
     path('update/profile', views.update_profile, name = 'updateProfile'),
-    path('post/project', views.post_project, name = 'postProject')
+    path('post/project', views.post_project, name = 'postProject'),
+    path('rating/<int:id>', views.ratings, name = 'ratings'),
+    path('search/', views.search_project, name = 'search_project'),
+
     
 ]
 if settings.DEBUG:
