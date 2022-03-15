1f35c2d670ddf4e1c0f0ba552e1dfa1cecc753f6
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-16 00:24:53 +0300

    Added a ratings form

--- ratings/forms.py
+++ ratings/forms.py
@@ -1,6 +1,6 @@
 from pyexpat import model
 from django import forms
-from .models import Profile, Project
+from .models import Profile, Project, Ratings
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,8 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['profile']
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Ratings
+        exclude = ['project', 'user', 'average']
