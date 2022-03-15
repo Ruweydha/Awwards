f8737cea3592af1739cb78bd6d868161cf4b8b8b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-16 00:26:41 +0300

    Created a view for rating projects

--- ratings/views.py
+++ ratings/views.py
@@ -1,8 +1,11 @@
+from email import message
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from ratings.models import Profile, Project
-from .forms import UpdateProfileForm,ProjectForm
+from ratings.models import Profile, Ratings, Project
+from .forms import UpdateProfileForm,ProjectForm, RatingForm
+from django.core.exceptions import PermissionDenied
+
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -10,7 +13,7 @@ from .forms import UpdateProfileForm,ProjectForm
 def home(request):
    current_user = request.user
    projects = Project.objects.all()
-   form = UpdateProfileForm()
+   form = RatingForm()
    return render(request,'home.html', {"current_user": current_user, "form": form, "projects":projects})
 
 def update_profile(request):
@@ -51,3 +54,50 @@ def post_project(request):
 
     return render(request, 'post_project.html', {"current_user":current_user , "form":form})    
 
+def ratings(request, id):
+    current_user = request.user
+    project = Project.objects.filter(pk = id).first()
+    ratings = Ratings.objects.filter(project = project, user = current_user)
+    
+    if request.method == 'POST':
+        if ratings:
+            raise PermissionDenied('You have rated this project.')        
+        else:
+            form = RatingForm(request.POST, request.FILES)
+            if form.is_valid():
+                rating = form.save(commit = False)
+                design = form.cleaned_data['design']
+                usability = form.cleaned_data['usability']
+                content = form.cleaned_data['content']
+                rating.user = current_user
+                rating.project = project
+                rating.average = (float(design) + float(usability) + float(content)) /3
+                rating.save()
+
+            return redirect("home")   
+
+    else:
+        form = RatingForm()  
+
+    return render(request, 'ratings.html', {"current_user":current_user , "form":form, "project":project})    
+
+def search_project (request):
+    if 'project' in request.GET and request.GET["project"]:
+        search_project = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_project)
+        message = f'{search_project}'
+
+        return render(request, 'search.html',{"message":message,"results": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+
+
+
+    
+
+
+    
