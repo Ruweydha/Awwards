f3d137de49ec5d9e9a94d1f95fe07d3905eadb33
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-16 05:44:15 +0300

    Added some fields

--- ratings/serializer.py
+++ ratings/serializer.py
@@ -5,9 +5,9 @@ from .models import Profile, Project
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('user', 'bio', 'projects', 'profile_pic')
+        fields = ('user', 'bio', 'profile_pic', 'contact')
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('title', 'description', 'image', 'profile')
+        fields = ('title', 'description', 'image', 'profile', 'project_url', 'date_posted')
